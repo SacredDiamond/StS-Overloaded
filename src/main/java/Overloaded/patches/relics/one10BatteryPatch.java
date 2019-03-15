0ca95de94a33eb7d5e19f4b34ae235b4cdbd7f25
SacredDiamond <45544974+SacredDiamond@users.noreply.github.com>
2019-03-15 11:05:17 -0400

    sad

--- src/main/java/Overloaded/patches/relics/one10BatteryPatch.java
+++ src/main/java/Overloaded/patches/relics/one10BatteryPatch.java
@@ -10,6 +10,9 @@ import com.megacrit.cardcrawl.powers.*;
 import javassist.*;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 
+import java.util.logging.Logger;
+
+import static Overloaded.OverloadedBase.logger;
 
 
 @SpirePatch(clz = DrawCardAction.class, method = "update")
@@ -21,12 +24,26 @@ public class one10BatteryPatch {
     @SpireInsertPatch(locator = Locator.class)
 
     public static void Insert(AbstractGameAction self) {
+
+        System.out.println("player has 110% battery? | " + AbstractDungeon.player.hasRelic(one10Battery.ID));
+        logger.info("player has 110% battery? | " + AbstractDungeon.player.hasRelic(one10Battery.ID) );
+        logger.info("what is self? | " + self);
+        logger.info("how much is self.amount? | " + self.amount);
+
         if (AbstractDungeon.player.hasRelic(one10Battery.ID)) {
             AbstractDungeon.player.getRelic(one10Battery.ID).flash();
             AbstractCard top;
+
+            logger.info("-.-.-.- entering loop -.-.-.-.-");
             for(int i = 0; i<self.amount;i++) {
+                logger.info("what is self? | " + self);
+                logger.info("how much is self.amount? | " + self.amount);
+                logger.info("what is i? | " + i);
+
                 top = AbstractDungeon.player.drawPile.getTopCard();
-                AbstractDungeon.actionManager.addToBottom(new DiscardSpecificCardAction(top));
+                logger.info("what is the yop card of the deck? | " + top);
+
+                AbstractDungeon.actionManager.addToBottom(new DiscardSpecificCardAction(top, AbstractDungeon.player.drawPile));
                 AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(top.makeStatEquivalentCopy(), 1, true, true, false));
             }
         }
