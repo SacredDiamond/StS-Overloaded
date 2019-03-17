2fc779f2fd1d2891e9d5ea9aa2cf379bd9e339d1
SacredDiamond <45544974+SacredDiamond@users.noreply.github.com>
2019-03-16 21:59:56 -0400

    almost

--- src/main/java/Overloaded/patches/relics/one10BatteryPatch.java
+++ src/main/java/Overloaded/patches/relics/one10BatteryPatch.java
@@ -6,16 +6,16 @@ import com.evacipated.cardcrawl.modthespire.lib.SpireInsertLocator;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DiscardSpecificCardAction;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.actions.common.*;
 import com.megacrit.cardcrawl.actions.defect.ScrapeAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
 
 import Overloaded.relics.one10Battery;
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndAddToDrawPileEffect;
 import javassist.CtBehavior;
 
 import java.util.logging.Logger;
@@ -29,8 +29,6 @@ public class one10BatteryPatch {
 
     @SpireInsertPatch(locator = Locator.class)
     public static void Insert(AbstractGameAction self) {
-        if (AbstractDungeon.player.hasRelic(one10Battery.ID)) {
-            AbstractDungeon.player.getRelic(one10Battery.ID).flash();
 
             logger.info("player has 110% battery? | " + AbstractDungeon.player.hasRelic(one10Battery.ID) );
             logger.info("what is self? | " + self);
@@ -39,6 +37,7 @@ public class one10BatteryPatch {
             if (AbstractDungeon.player.hasRelic(one10Battery.ID)) {
                 AbstractDungeon.player.getRelic(one10Battery.ID).flash();
                 AbstractCard top;
+                CardGroup draw = AbstractDungeon.player.drawPile;
 
                 logger.info("-.-.-.- entering loop -.-.-.-.-");
                 for(int i = 0; i<self.amount;i++) {
@@ -46,26 +45,25 @@ public class one10BatteryPatch {
                     logger.info("how much is self.amount? | " + self.amount);
                     logger.info("what is i? | " + i);
 
-                    top = AbstractDungeon.player.drawPile.getTopCard();
+                    top = draw.group.get(draw.group.size() - 1 - i);
                     logger.info("what is the top card of the deck? | " + top);
 
-                    AbstractDungeon.actionManager.addToBottom(new DiscardSpecificCardAction(top, AbstractDungeon.player.drawPile));
+                    //AbstractDungeon.effectList.add(new ShowCardAndAddToDrawPileEffect(AbstractDungeon.player.drawPile.getTopCard(), true, false));
+                    //AbstractDungeon.player.drawPile.getTopCard().moveToDiscardPile();
+
+                    AbstractDungeon.actionManager.addToBottom(new DiscardSpecificCardAction(top, draw));
                     AbstractDungeon.actionManager.addToBottom(new MakeTempCardInDrawPileAction(top.makeStatEquivalentCopy(), 1, true, true, false));
                 }
 
-                logger.info("player has 110% battery? | " + AbstractDungeon.player.hasRelic(one10Battery.ID) );
-                logger.info("what is self? | " + self);
-                logger.info("how much is self.amount? | " + self.amount);
             }
 
         }
-    }
 
     private static class Locator extends SpireInsertLocator {
         @Override
         public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
             Matcher finalMatcher = new Matcher.MethodCallMatcher("com.megacrit.cardcrawl.characters.AbstractPlayer", "createHandIsFullDialog");
-            return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+            return LineFinder.findAllInOrder(ctMethodToPatch, finalMatcher);
         }
     }
 }
