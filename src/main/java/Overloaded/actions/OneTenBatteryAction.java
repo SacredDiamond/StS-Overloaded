29f4b42d0c5278f9feba890b8d2345e613e43b12
reina <ReinaSHSL@users.noreply.github.com>
2019-03-22 14:52:47 -0700

    action

---
+++ src/main/java/Overloaded/actions/OneTenBatteryAction.java
@@ -0,0 +1,26 @@
+package Overloaded.actions;
+
+import basemod.BaseMod;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class OneTenBatteryAction extends AbstractGameAction {
+
+    public OneTenBatteryAction(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public void update() {
+        for (int i = 0; i < amount; i++) {
+            if (i < AbstractDungeon.player.drawPile.size()) {
+                AbstractCard cardToDupe = AbstractDungeon.player.drawPile.getNCardFromTop(i);
+                AbstractDungeon.player.drawPile.moveToDiscardPile(cardToDupe);
+                AbstractDungeon.actionManager.addToTop(new MakeTempCardInDrawPileAction(cardToDupe, 1, true, true));
+            }
+        }
+        isDone = true;
+    }
+}
--- src/main/java/Overloaded/patches/relics/one10BatteryPatch.java
+++ src/main/java/Overloaded/patches/relics/one10BatteryPatch.java
@@ -1,10 +1,8 @@
 package Overloaded.patches.relics;
 
-import com.evacipated.cardcrawl.modthespire.lib.LineFinder;
-import com.evacipated.cardcrawl.modthespire.lib.Matcher;
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertLocator;
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import Overloaded.actions.OneTenBatteryAction;
+import basemod.BaseMod;
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.*;
 import com.megacrit.cardcrawl.actions.defect.ScrapeAction;
@@ -35,12 +33,14 @@ public class one10BatteryPatch {
 
 
     @SpireInsertPatch(locator = Locator.class)
-    public static void Insert(AbstractGameAction __instance) {
+    public static SpireReturn Insert(AbstractGameAction __instance) {
         if (AbstractDungeon.player.hasRelic(one10Battery.ID)) {
             AbstractDungeon.player.getRelic(one10Battery.ID).flash();
-
+            int amountOfCardsOverDrawn = __instance.amount - BaseMod.DEFAULT_MAX_HAND_SIZE;
+            AbstractDungeon.actionManager.addToTop(new OneTenBatteryAction(amountOfCardsOverDrawn));
+            return SpireReturn.Return(null);
         }
-
+        return SpireReturn.Continue();
     }
 
     private static class Locator extends SpireInsertLocator {
