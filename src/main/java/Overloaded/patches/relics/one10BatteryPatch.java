0537bdb0f0e58e6514a8b740886effc9c021fe08
reina <ReinaSHSL@users.noreply.github.com>
2019-03-22 14:30:33 -0700

    use .class

--- src/main/java/Overloaded/patches/relics/one10BatteryPatch.java
+++ src/main/java/Overloaded/patches/relics/one10BatteryPatch.java
@@ -36,7 +36,6 @@ public class one10BatteryPatch {
 
     @SpireInsertPatch(locator = Locator.class)
     public static void Insert(AbstractGameAction __instance) {
-
         if (AbstractDungeon.player.hasRelic(one10Battery.ID)) {
             AbstractDungeon.player.getRelic(one10Battery.ID).flash();
 
@@ -47,7 +46,7 @@ public class one10BatteryPatch {
     private static class Locator extends SpireInsertLocator {
         @Override
         public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
-            Matcher finalMatcher = new Matcher.MethodCallMatcher("com.megacrit.cardcrawl.characters.AbstractPlayer", "createHandIsFullDialog");
+            Matcher finalMatcher = new Matcher.MethodCallMatcher(AbstractPlayer.class, "createHandIsFullDialog");
             return LineFinder.findAllInOrder(ctMethodToPatch, finalMatcher);
         }
     }
