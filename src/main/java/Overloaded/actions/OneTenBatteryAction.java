6efb0cf69cc039dc139416a1a5281386c011dee8
SacredDiamond <45544974+SacredDiamond@users.noreply.github.com>
2019-04-03 13:43:08 -0400

    f

--- src/main/java/Overloaded/actions/OneTenBatteryAction.java
+++ src/main/java/Overloaded/actions/OneTenBatteryAction.java
@@ -2,7 +2,10 @@ package Overloaded.actions;
 
 import basemod.BaseMod;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.EmptyDeckShuffleAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.actions.common.ShuffleAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
@@ -14,13 +17,18 @@ public class OneTenBatteryAction extends AbstractGameAction {
 
     @Override
     public void update() {
+
+
         for (int i = 0; i < amount; i++) {
-            if (i < AbstractDungeon.player.drawPile.size()) {
+
+           if (i < AbstractDungeon.player.drawPile.size()) {
                 AbstractCard cardToDupe = AbstractDungeon.player.drawPile.getNCardFromTop(i);
                 AbstractDungeon.player.drawPile.moveToDiscardPile(cardToDupe);
                 AbstractDungeon.actionManager.addToTop(new MakeTempCardInDrawPileAction(cardToDupe, 1, true, true));
             }
+
         }
+
         isDone = true;
     }
 }
--- src/main/java/Overloaded/patches/relics/one10BatteryPatch.java
+++ src/main/java/Overloaded/patches/relics/one10BatteryPatch.java
@@ -33,14 +33,22 @@ public class one10BatteryPatch {
 
 
     @SpireInsertPatch(locator = Locator.class)
-    public static SpireReturn Insert(AbstractGameAction __instance) {
+    public static void Insert(AbstractGameAction __instance) {
         if (AbstractDungeon.player.hasRelic(one10Battery.ID)) {
             AbstractDungeon.player.getRelic(one10Battery.ID).flash();
+
             AbstractDungeon.actionManager.addToTop(new OneTenBatteryAction(__instance.amount));
-            __instance.isDone = true;
-            return SpireReturn.Return(null);
+
+            if((AbstractDungeon.player.drawPile.size() < __instance.amount) &&(AbstractDungeon.player.discardPile.size() > 0) ){
+
+                AbstractDungeon.actionManager.addToTop(new EmptyDeckShuffleAction());
+                AbstractDungeon.actionManager.addToTop(new ShuffleAction(AbstractDungeon.player.drawPile, false));
+            }
+
+          //  __instance.isDone = true;
+          //  return SpireReturn.Return(null);
         }
-        return SpireReturn.Continue();
+      //  return SpireReturn.Continue();
     }
 
     private static class Locator extends SpireInsertLocator {
